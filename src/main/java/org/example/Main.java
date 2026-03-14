0a30a5b9baaf95be9d6529c92d94d325a306f201
wk333cs <wito.karpinski@gmail.com>
2026-03-14 21:20:22 +0100

    Third try

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,6 @@
 package org.example;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Scanner;
 
 
 public class Main {
@@ -14,23 +12,42 @@ public class Main {
             createProfTable(conn);
             createDocTable(conn);
 
+           // Placeholder for adding profiles
+            Scanner sc = new Scanner(System.in);
+            String a = sc.nextLine();
+            addProfile(conn, a);
+            String b = sc.nextLine();
+            addProfile(conn, b);
+
+
+            /*
+            Placeholder for adding documents
+             */
+            addDocument( conn, 5, "Hilary", "Okulicki", 'd', 1905, "Kutno","Kutno" ,  null, "ffm", "lubił koty");
+
 
         } catch (SQLException e) {
             System.out.println("Database error: " + e.getMessage());
         }
 
     }
+
     public static void createProfTable(Connection conn) throws SQLException {
         String sql = """
-               CREATE TABLE IF NOT EXISTS profiles(
+               
+                CREATE TABLE IF NOT EXISTS profiles(
                id INTEGER PRIMARY KEY AUTOINCREMENT,
-               profileName TEXT NOT NULL
-             
-             );
+               profileName TEXT UNIQUE NOT NULL
+                                          );
                 """;
         try(Statement stnt = conn.createStatement()) {
-            stnt.execute(sql);
-        }
+            stnt.
+
+        execute(
+
+    sql);
+
+    }
     }
     public static void createDocTable(Connection conn) throws SQLException{
         String sql = """
@@ -49,9 +66,77 @@ public class Main {
                 isPinned BOOLEAN NOT NULL,
                 FOREIGN KEY (profileID) REFERENCES profiles(id)
                 );
-                """;
+                 """;
         try(Statement stnt = conn.createStatement()) {
             stnt.execute(sql);
+        }
+    }
+
+    public static void addProfile(Connection conn, String profileName) throws SQLException {
+        String check = "SELECT 1 FROM profiles WHERE profileName = ? LIMIT 1";
+        try(PreparedStatement ps = conn.prepareStatement(check)){
+            ps.setString(1,profileName);
+
+            try (ResultSet rs = ps.executeQuery()) { //checks if profile is unique
+                if (rs.next()) {
+                System.out.println("Profile exists already"); //placeholder, normally method to send error message
+                } else {
+                    String sql = "INSERT INTO profiles(profileName) VALUES (?)";
+                    try {
+                        conn.setAutoCommit(false);
+                        try (PreparedStatement ps2 = conn.prepareStatement(sql)) {
+                            ps2.setString(1, profileName);
+                            ps2.executeUpdate();
+                        }
+                        conn.commit();
+                    } catch (SQLException e) {
+                        try {
+                            conn.rollback();
+                        } catch (SQLException ignored) {}
+
+                    } finally {
+                        try {
+                            conn.setAutoCommit(true);
+                        } catch (SQLException ignored) {}
+                    }
+                }
+            }
+
+        }
+
+
+
+    }
+
+    public static void addDocument(Connection conn, int profile, String name, String surname, char type, int year, String parish, String city, String village, String branch, String info) throws SQLException{
+       String sql = "INSERT INTO documents(profileID, name, surname, type, year, parish, city, village, branch, info, isPinned) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, false)";
+
+        try {
+            conn.setAutoCommit(false);
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setInt(1, profile);
+                ps.setString(2, name);
+                ps.setString(3, surname);
+                ps.setString(4, String.valueOf(type));
+                ps.setInt(5, year);
+                ps.setString(6, parish);
+                ps.setString(7, city);
+                ps.setString(8, village);
+                ps.setString(9, branch);
+                ps.setString(10, info);
+
+                ps.executeUpdate();
+            }
+            conn.commit();
+        } catch (SQLException e) {
+            try {
+                conn.rollback();
+            } catch (SQLException ignored) {}
+
+        } finally {
+            try {
+                conn.setAutoCommit(true);
+            } catch (SQLException ignored) {}
         }
     }
 }
